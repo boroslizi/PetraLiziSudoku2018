2c11f9f58fd9be3be5c013305cf5dcaab340b49d
Lizi Boros <boroslizi@gmail.com>
2018-11-13 16:06:08 +0100

    COLORS list

--- sudoku_levels_works.py
+++ sudoku_levels_works.py
@@ -5,25 +5,15 @@ from copy import deepcopy
 from grids import easy, medium, hard, win0, winwin
 os.system("clear")
 
-# original numbers color (lol, álmodik a nyomor)
-cbegin = "\033[34m"
-cend = "\033[0m"
+COLORS = [
+    "\033[91m",  # red
+    "\033[34m",  # blue
+    "\033[90m",  # grey
+    "\033[0m",  # white
+    ]
 
-# grid color
-gbegin = "\033[90m"
-gend = "\033[0m"
-
-# error color
-ebegin = "\033[91m"
-eend = "\033[0m"
-
-
-
-# original grid, not to be modified
-grid0 = []
-
-# second grid, zeros can be modified
-grid = []
+grid0 = []  # original grid, not to be modified
+grid = []  # second grid, zeros can be modified
 
 # row and column indexing
 index_cap = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9}
@@ -40,50 +30,55 @@ error7 = "\nPlease enter requested characters."
 
 
 # middle part of long print line in print_sudoku
-def long_line():
-    return (" {} " + gbegin + "│" + gend + " {} " + gbegin + "│" + gend + " {} " + gbegin + "║" + gend)
+def long_line(c1, c2):
+    return (" {} " + c1 + "│" + c2 + " {} " + c1 + "│" + c2 + " {} " + c1 + "║" + c2)
 
 
 # printing the original grid
-def print_sudoku(board):
-    print(gbegin + "\n    a   b   c   d   e   f   g   h   i  " + gend)
-    print(gbegin + "  ╔" + ("═══╤═══╤═══╦"*2) + "═══╤═══╤═══╗" + gend)
+def print_sudoku(board, c_grid, c_end):
+    print(c_grid + "\n    a   b   c   d   e   f   g   h   i  " + c_end)
+    print(c_grid + "  ╔" + ("═══╤═══╤═══╦"*2) + "═══╤═══╤═══╗" + c_end)
     for i, row in enumerate(board):
-            print((gbegin + "{} ║" + gend + (long_line())*3).format(*[x if x != 0 else " " for x in row]))
+
+            # format comprehension function
+            def compr():
+                return ([x if x != 0 else " " for x in row])
+
+            print((c_grid + "{} ║" + c_end + (long_line(COLORS[2], COLORS[3]))*3).format(*compr()))
             if i % 3 == 2 and i < 8:
-                print(gbegin + "  ╠" + ("═══╪═══╪═══╬"*2) + "═══╪═══╪═══╣" + gend)
+                print(c_grid + "  ╠" + ("═══╪═══╪═══╬"*2) + "═══╪═══╪═══╣" + c_end)
             elif i == 8:
-                print(gbegin + "  ╚" + ("═══╧═══╧═══╩"*2) + "═══╧═══╧═══╝" + gend)
+                print(c_grid + "  ╚" + ("═══╧═══╧═══╩"*2) + "═══╧═══╧═══╝" + c_end)
             else:
-                print(gbegin + "  ╟" + ("───┼───┼───╫")*2 + "───┼───┼───╢" + gend)
+                print(c_grid + "  ╟" + ("───┼───┼───╫")*2 + "───┼───┼───╢" + c_end)
 
 
 # printing a new grid after adding a new number
-def grid_new(board):
+def grid_new(board, c_error, c_end):
     row_in = str(input("\nSelect a row (A - I): "))
     column_in = str(input("Select a column (a - i): "))
     num = int(input("Enter a number (1-9): "))
     if num in board[int(index_cap[row_in]) - 1]:  # checking row
         os.system("clear")
-        print(ebegin + error1 + eend)
+        print(c_error + error1 + c_end)
     elif num in [col[int(index_small[column_in])] for col in board]:  # checking column
         os.system("clear")
-        print(ebegin + error2 + eend)
+        print(c_error + error2 + c_end)
     elif num > 0 and num < 10:  # checking if number is between 1 and 9
         if grid0[int(index_cap[row_in]) - 1][int(index_small[(column_in)])] == 0:
             board[int(index_cap[row_in]) - 1][int(index_small[(column_in)])] = num
             os.system("clear")
         else:
             os.system("clear")
-            print(ebegin + error3 + eend)
+            print(c_error + error3 + c_end)
     else:
         os.system("clear")
-        print(ebegin + error4 + eend)
+        print(c_error + error4 + c_end)
     return board
 
 
 # deleting a number
-def grid_delete(board):
+def grid_delete(board, c_error, c_end):
     print("\nDefine place for number to delete.")
     row_in = str(input("\nSelect a row (A - I): "))
     column_in = str(input("Select a column (a - i): "))
@@ -92,7 +87,7 @@ def grid_delete(board):
         os.system("clear")
     else:
         os.system("clear")
-        print(ebegin + error3 + eend)
+        print(c_error + error3 + c_end)
     return board
 
 
@@ -111,28 +106,16 @@ while True:
             action2 = int(input("\nChoose a level! "))
             if action2 == 1:
                 grid0 = easy
-                grid = deepcopy(grid0)
-                os.system("clear")
-                print_sudoku(grid0)
-                break
             elif action2 == 2:
                 grid0 = medium
-                grid = deepcopy(grid0)
-                os.system("clear")
-                print_sudoku(grid0)
-                break
             elif action2 == 3:
                 grid0 = hard
-                grid = deepcopy(grid0)
-                os.system("clear")
-                print_sudoku(grid0)
-                break
             elif action2 == 4:
                 grid0 = win0
-                grid = deepcopy(grid0)
-                os.system("clear")
-                print_sudoku(grid0)
-                break
+            grid = deepcopy(grid0)
+            os.system("clear")
+            print_sudoku(grid0, COLORS[2], COLORS[3])
+            break
         elif action == 2:
             os.system("clear")
             file = "saved_sudoku.pickle"
@@ -140,14 +123,14 @@ while True:
                 grid0 = pickle.load(f)
                 grid = deepcopy(grid0)
                 os.system("clear")
-                print_sudoku(grid0)
+                print_sudoku(grid0, COLORS[2], COLORS[3])
                 break
         else:
             os.system("clear")
-            print(ebegin + error5 + eend)
+            print(COLORS[0] + error5 + COLORS[3])
     except ValueError:
         os.system("clear")
-        print(ebegin + error5 + eend)
+        print(COLORS[0] + error5 + COLORS[3])
 
 
 # menu3
@@ -171,11 +154,11 @@ while True:
             action3 = int(input("\nWhat would you like to do? "))
             try:
                 if action3 == 1:
-                    grid = grid_new(grid)
-                    print_sudoku(grid)
+                    grid = grid_new(grid, COLORS[0], COLORS[3])
+                    print_sudoku(grid, COLORS[2], COLORS[3])
                 elif action3 == 2:
-                    grid = grid_delete(grid)
-                    print_sudoku(grid)
+                    grid = grid_delete(grid, COLORS[0], COLORS[3])
+                    print_sudoku(grid, COLORS[2], COLORS[3])
                 elif action3 == 3:
                     file = "saved_sudoku.pickle"
                     with open(file, "wb") as f:
@@ -187,23 +170,23 @@ while True:
                         break
                     elif saving == "n":
                         os.system("clear")
-                        print_sudoku(grid)
+                        print_sudoku(grid, COLORS[2], COLORS[3])
                     else:
                         os.system("clear")
                         print(ebegin + error6 + eend)
-                        print_sudoku(grid)
+                        print_sudoku(grid, COLORS[2], COLORS[3])
                 elif action3 == 4:
                     break
                 else:
                     os.system("clear")
                     print(ebegin + error5 + eend)
-                    print_sudoku(grid)
+                    print_sudoku(grid, COLORS[2], COLORS[3])
             except ValueError:
                 os.system("clear")
                 print(ebegin + error7 + eend)
-                print_sudoku(grid)
+                print_sudoku(grid, COLORS[2], COLORS[3])
         except ValueError:
             os.system("clear")
             os.system("clear")
             print(ebegin + error5 + eend)
-            print_sudoku(grid)
+            print_sudoku(grid, COLORS[2], COLORS[3])
